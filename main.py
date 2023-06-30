157f627f99b0b1280042c8b2002d24a9876808d5
AleksSM <aleks-m83@mail.ru>
2023-06-30 20:35:08 +0400

    add func create table in db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sqlalchemy as sq
 from database.database import Database
 
 if __name__ == "__main__":
     base = Database()
-    print(base.create_conect())
+    base.create_tables(sq.create_engine(base.create_conect()))
