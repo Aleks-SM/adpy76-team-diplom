56023547185d7f4e9141a03aac52f46611ffdf7c
AleksSM <aleks-m83@mail.ru>
2023-07-01 10:05:56 +0400

    update main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sqlalchemy as sq
 from database.database import Database
+from database.requests import get_user_data
 
 if __name__ == "__main__":
     base = Database()
-    base.create_tables(sq.create_engine(base.create_conect()))
+    # base.create_tables(sq.create_engine(base.create_conect()))
+    engine = sq.create_engine(base.create_conect())
+    get_user_data(engine, 1)
