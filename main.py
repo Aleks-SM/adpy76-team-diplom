e05a8337f272eafb869c5986bf63d259addd8c44
AleksSM <aleks-m83@mail.ru>
2023-07-03 19:50:09 +0400

    update main

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from database.requests import get_user_data
 
 if __name__ == "__main__":
     base = Database()
-    # base.create_tables(sq.create_engine(base.create_conect()))
-    get_user_data(1)
+    # base.create_tables(base.create_conect())
+    print(get_user_data(1))
