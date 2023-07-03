a5765f2c2ff1a9bde310bca051c3d4b9910269a7
AleksSM <aleks-m83@mail.ru>
2023-07-03 19:51:01 +0400

    update func get_user_data

--- database/requests.py
+++ database/requests.py
@@ -1,26 +1,29 @@
 from database.db_dataclassess import ClientUser
 from sqlalchemy.orm import sessionmaker
 from database.models import User, Favorite, Blacklist
+from database.database import Database
+from database.db_dataclassess import ClientUser
 
 
 #Должна возращать ClientUser
 def get_user_data(user_id: int) -> ClientUser:
-    Session = sessionmaker(bind=engine)
+    # engine = Database().create_conect()
+    Session = sessionmaker(bind=Database().create_conect())
     session = Session()
 
     for query in session.query(User).filter(User.user_id == user_id).all():
-        query_result = {'user_id': query.user_id,
-                        'search_age_min': query.search_age_min,
-                        'search_age_max': query.search_age_max,
-                        'gender': query.search_gender,
-                        'city': query.search_city,
-                        'state': query.state}
-    return query_result
+        self = ClientUser(user_id=query.user_id,
+                   age_min=query.search_age_min,
+                   age_max=query.search_age_max,
+                   gender=query.search_gender,
+                   city=query.search_city,
+                   state=query.state)
+    return self
 
 
 # Должна возвращать сет из blocked_vk_user_id
 def get_user_blacklist() -> set[int]:
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=Database().create_conect())
     session = Session()
     for query in session.query(User).join(Blacklist).filter(User.user_id == user_id).all():
         query_result = {"user_id": query.user_id,
