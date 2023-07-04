9ea2dc32b9f22d61aeefcc2219e92cbc219ae7aa
AleksSM <aleks-m83@mail.ru>
2023-07-04 19:40:32 +0400

    update models

--- database/models.py
+++ database/models.py
@@ -9,10 +9,10 @@ class User(Base):
 
     user_id = sq.Column(sq.BigInteger, primary_key=True)
     user_name = sq.Column(sq.String(length=50), unique=True)
-    search_gender = sq.Column(sq.Integer, nullable=False)
+    search_gender = sq.Column(sq.Integer)
     search_age_min = sq.Column(sq.Integer, nullable=False)
     search_age_max = sq.Column(sq.Integer, nullable=False)
-    search_city = sq.Column(sq.String(length=50), nullable=False)
+    search_city = sq.Column(sq.String(length=50))
     state = sq.Column(sq.Integer)
 
 class Favorite(Base):
