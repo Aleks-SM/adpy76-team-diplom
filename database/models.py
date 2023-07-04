f39c7c5d55e6489d9967394cd41f7b81ca0171cb
AleksSM <aleks-m83@mail.ru>
2023-07-04 19:59:35 +0400

    update models

--- database/models.py
+++ database/models.py
@@ -1,4 +1,5 @@
-import sqlalchemy as sq
+from sqlalchemy import Column, BigInteger, Integer, String
+from sqlalchemy.schema import ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import declarative_base
 
@@ -7,28 +8,28 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "user"
 
-    user_id = sq.Column(sq.BigInteger, primary_key=True)
-    user_name = sq.Column(sq.String(length=50), unique=True)
-    search_gender = sq.Column(sq.Integer)
-    search_age_min = sq.Column(sq.Integer, nullable=False)
-    search_age_max = sq.Column(sq.Integer, nullable=False)
-    search_city = sq.Column(sq.String(length=50))
-    state = sq.Column(sq.Integer)
+    user_id = Column(BigInteger, primary_key=True)
+    user_name = Column(String(length=50), unique=True)
+    search_gender = Column(Integer)
+    search_age_min = Column(Integer, nullable=False)
+    search_age_max = Column(Integer, nullable=False)
+    search_city = Column(String(length=50))
+    state = Column(Integer)
 
 class Favorite(Base):
     __tablename__ = "favorite"
 
-    favorite_id = sq.Column(sq.Integer, primary_key=True)
-    user_id = sq.Column(sq.BigInteger, sq.ForeignKey("user.user_id"), nullable=False)
-    favorite_vk_user_id = sq.Column(sq.BigInteger, nullable=True)
+    favorite_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, ForeignKey("user.user_id"), nullable=False)
+    favorite_vk_user_id = Column(BigInteger, nullable=True)
 
     favorite = relationship(User, backref="user_fav")
 
 class Blacklist(Base):
     __tablename__ = "blacklist"
 
-    blacklist_id = sq.Column(sq.Integer, primary_key=True)
-    user_id = sq.Column(sq.BigInteger, sq.ForeignKey("user.user_id"), nullable=False)
-    blocked_vk_user_id = sq.Column(sq.Integer, nullable=True)
+    blacklist_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, ForeignKey("user.user_id"), nullable=False)
+    blocked_vk_user_id = Column(Integer, nullable=True)
 
     user = relationship(User, backref="user_black")
