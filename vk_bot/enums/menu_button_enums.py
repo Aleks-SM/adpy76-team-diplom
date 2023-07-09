4406b7981309e7e612bdb248e8fe616990d8f6a3
ravThirst <richardradchenko@gmail.com>
2023-07-09 23:53:36 +1000

    some fixes

--- vk_bot/enums/menu_button_enums.py
+++ vk_bot/enums/menu_button_enums.py
@@ -1,7 +1,7 @@
 class MenuButtonEnum:
-    NEXT: 0
-    SHOW_FAVORITES: 1
-    LIKE: 2
-    BLOCK_USER: 3
-    SEARCH: 4
-    PLAIN_TEXT: 5
+    NEXT = 0
+    SHOW_FAVORITES = 1
+    LIKE = 2
+    BLOCK_USER = 3
+    SEARCH = 4
+    PLAIN_TEXT = 5
--- vk_bot/states/enums/state_enum.py
+++ vk_bot/states/enums/state_enum.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class StateEnum(Enum):
-    ASK_AGE_MIN: 0
-    ASK_AGE_MAX: 1
-    ASK_CITY: 2
-    ASK_GENDER: 3
-    REGISTERED: 4
+    ASK_AGE_MIN = 0
+    ASK_AGE_MAX = 1
+    ASK_CITY = 2
+    ASK_GENDER = 3
+    REGISTERED = 4
