40377cbe843b409b2e46cee69f4f5f8a0068c906
Mica78 <mica78@yandex.ru>
2023-07-10 22:31:41 +0400

    update Talker.vk_search_user send attachment

--- vk_api/talker.py
+++ vk_api/talker.py
@@ -1,8 +1,9 @@
 import os
-
+import aiohttp
 from vk_bot.user.user import VkUserSearch
 from vkbottle.bot import Bot, Message, MessageEvent, rules
 from vkbottle import Keyboard, KeyboardButtonColor, Text, Callback, GroupEventType
+from tools import get_attachment_for_vk_bot
 
 
 class Talker:
@@ -30,11 +31,26 @@ class Talker:
         await bot.api.messages.send(
             text=f"Результат поиска\n{vk_user_data.name}\n{vk_user_data.profile_link}"
         )
-        for photo in vk_user_data.photos:
-            await bot.api.messages.send(attachment=photo)
-        if vk_user_data.related_photos:
-            for related_photo in vk_user_data.related_photos:
-                await bot.api.messages.send(attachment=related_photo)
+        async with aiohttp.ClientSession as session:
+            for photo_link in vk_user_data.photos:
+                attachment_photo = await get_attachment_for_vk_bot(
+                    session,
+                    photo_link,
+                    bot,
+                    user_id=self.user_id
+                )
+                await bot.api.messages.send(attachment=attachment_photo)
+                await bot.api.messages.send(content_source=photo_link)
+            if vk_user_data.related_photos:
+                for related_photo_link in vk_user_data.related_photos:
+                    attachment_related_photo = await get_attachment_for_vk_bot(
+                        session,
+                        related_photo_link,
+                        bot,
+                        user_id=self.user_id
+                    )
+                    await bot.api.messages.send(attachment=attachment_related_photo)
+                    await bot.api.messages.send(content_source=related_photo_link)
 
     async def menu_buttons(self, bot=bot):
         keyboard = (
--- vk_api/tools.py
+++ vk_api/tools.py
@@ -1,6 +1,6 @@
 import os
-
 from vkbottle import API
+from io import BytesIO
 
 
 async def check_if_city_exists(city: str) -> bool:
@@ -18,3 +18,18 @@ async def check_if_city_exists(city: str) -> bool:
     # if cities_set:
     #     print(cities_set)
     #     return cities_set
+
+
+async def get_attachment_for_vk_bot(session, photo_link, bot, user_id):
+    async with session.get(photo_link) as response:
+        photo_file = await response.content.read()
+        file = BytesIO(photo_file)
+        bot_link = await bot.api.photos.get_messages_upload_server(peer_id=user_id)
+        async with session.post(bot_link.upload_url, data=file) as response1:
+            data = await response1.json()
+            saved_photo = await bot.api.photos.save_messages_photo(
+                server=data['server'],
+                photo=data['photo'],
+                hash=data['hash']
+            )
+            return f'photo-{saved_photo[3]}_{saved_photo[0]}'
