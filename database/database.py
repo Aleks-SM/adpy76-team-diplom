9e7cc1aad0b6475cc88288148cf657c5db66702e
AleksSM <aleks-m83@mail.ru>
2023-07-11 19:02:30 +0400

    update datebase

--- database/database.py
+++ database/database.py
@@ -40,5 +40,5 @@ class Database:
         return Session()
 
     def create_tables(self, engine):
-        Base.metadata.drop_all(engine)
+        # Base.metadata.drop_all(engine)
         Base.metadata.create_all(engine)
