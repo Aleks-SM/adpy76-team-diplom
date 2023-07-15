c6fe416b916f0035bcb454af1cba148a73a749a9
AleksSM <aleks-m83@mail.ru>
2023-07-15 14:39:18 +0400

    update main

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from vk_api.listener import bot
 
 if __name__ == "__main__":
     base = Database()
+    base.drop_tables(base.create_connect())
     base.create_tables(base.create_connect())
     bot.run_forever()
